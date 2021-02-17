1767b293a900fc2f3b20af93415a5875f807a868
Daniel Williams <dlw1ll27@gmail.com>
2021-02-17 12:03:53 -0800

    adds single options hash

--- store_item.rb
+++ store_item.rb
@@ -24,11 +24,19 @@ class Item
   attr_reader = :make, :model, :color, :price
   attr_writer = :price
 
-  def initialize(input_make, input_model, input_color, input_price)
-    @make = input_make
-    @model = input_model
-    @color = input_color
-    @price = input_price
+  # def initialize(input_make, input_model, input_color, input_price)
+  #   @make = input_make
+  #   @model = input_model
+  #   @color = input_color
+  #   @price = input_price
+  # end
+
+  # MARK: single options hash
+  def initialize(input_options)
+    @make = input_options[:make]
+    @model = input_options[:model]
+    @color = input_options[:color]
+    @price = input_options[:price]
   end
 
   # def make
@@ -50,9 +58,17 @@ class Item
   # def price=(price)
   #   @price = price
   # end
+
+  def print_info
+    puts "#{@color} #{@make} #{@model} is #{@price} dollars"
+  end
 end
 
-car1 = Item.new("Toyota", "Highlander", "red", 40000)
+# car1 = Item.new("Toyota", "Highlander", "red", 40000)
 # puts car1.make
 # puts car1.model
 # puts car1.price
+
+#MARK: single options hash
+car1 = Item.new(make: "Toyota", model: "Highlander", color: "red", price: 40000)
+puts car1.print_info
