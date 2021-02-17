7133bfd1ffac15057ad0ef942548c439fc247e0e
Daniel Williams <dlw1ll27@gmail.com>
2021-02-17 10:29:14 -0800

    adds 2 more item hashes

--- store_item.rb
+++ store_item.rb
@@ -1,4 +1,21 @@
 #this store sells cars
 item1 = {
   :make => "Toyota",
+  :model => "Highlander",
+  :color => "red",
+  :price => "40000",
+}
+
+item2 = {
+  make: "Kia",
+  model: "Telluride",
+  color: "black",
+  price: "38000",
+}
+
+item3 = {
+  make: "Hyundai",
+  model: "Palisade",
+  color: "white",
+  price: "39000",
 }
